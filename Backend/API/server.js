4c9788fb08b53acdc1d71b2e3375896ddf23d8e3
rushi-bhaskar <rushikeshbhaskar7@gmail.com>
2023-09-09 22:30:03 +0530

    changed origin to backend url

--- Backend/API/server.js
+++ Backend/API/server.js
@@ -26,7 +26,7 @@ const connect = async()=>{
 
 };
 
-app.use(cors({origin:"https://framelancer.vercel.app", credentials: true}));
+app.use(cors({origin:"https://frame-lancer2-wgiz.vercel.app/", credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
