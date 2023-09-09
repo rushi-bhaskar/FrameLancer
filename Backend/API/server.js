efc9d8577ebe4232f7b13704e23adcb9078ddf49
rushi-bhaskar <rushikeshbhaskar7@gmail.com>
2023-09-09 22:23:18 +0530

    changed origin to star

--- Backend/API/server.js
+++ Backend/API/server.js
@@ -26,7 +26,7 @@ const connect = async()=>{
 
 };
 
-app.use(cors({origin:"https://frame-lancer2-wgiz.vercel.app/", credentials: true}));
+app.use(cors({origin:"*", credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
