a0cc5d1da40c720649772786b7d0e4315344ced4
rushi-bhaskar <rushikeshbhaskar7@gmail.com>
2023-09-09 22:17:18 +0530

    updated origin in frontend and backend

--- Backend/API/server.js
+++ Backend/API/server.js
@@ -26,7 +26,7 @@ const connect = async()=>{
 
 };
 
-app.use(cors({origin:"http://localhost:5173", credentials: true}));
+app.use(cors({origin:"https://frame-lancer2-wgiz.vercel.app/", credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
--- Frontend/src/utils/newRequests.js
+++ Frontend/src/utils/newRequests.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL:"http://localhost:8800/api/",
+    baseURL:"https://frame-lancer2-wgiz.vercel.app/api/",
     withCredentials:true,
 });
 
