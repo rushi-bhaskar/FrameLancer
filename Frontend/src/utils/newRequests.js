7889243c07b07b066b96ebb7c11716fb40a4b035
rushi-bhaskar <rushikeshbhaskar7@gmail.com>
2023-09-09 22:58:47 +0530

    changed withCredentials to false

--- Frontend/src/utils/newRequests.js
+++ Frontend/src/utils/newRequests.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
     baseURL:"https://frame-lancer2-wgiz.vercel.app/api/",
-    withCredentials:true,
+    withCredentials:false,
 });
 
 export default newRequest;
