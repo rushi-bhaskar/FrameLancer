4c06d3079851234f2b12208561d6e3f439c1df38
rushi-bhaskar <rushikeshbhaskar7@gmail.com>
2023-09-09 23:03:35 +0530

    changed api to API in baseURL

--- Frontend/src/utils/newRequests.js
+++ Frontend/src/utils/newRequests.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL:"https://frame-lancer2-wgiz.vercel.app/api/",
+    baseURL:"https://frame-lancer2-wgiz.vercel.app/API/",
     withCredentials:false,
 });
 
